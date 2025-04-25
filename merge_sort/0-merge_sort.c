a894da997e63589d347ab2f18117d4ee9c882aad
Tristan Duchamp <tristan.duchamp@medelse.com>
2025-04-25 10:59:15 +0200

    add 0

--- merge_sort/0-merge_sort.c
+++ merge_sort/0-merge_sort.c
@@ -3,86 +3,111 @@
 
 /**
  * merge - Merges two subarrays into one sorted array
+ * @array: Original array containing subarrays to merge
+ * @temp: Temporary array to store the merged result
+ * @left: Start index of left subarray
+ * @mid: End index of left subarray
+ * @right: End index of right subarray
  */
 void merge(int *array, int *temp, size_t left, size_t mid, size_t right)
 {
-    size_t i = left;
-    size_t j = mid;
-    size_t k = left;
-    
+    size_t i = left;    /* Index for left subarray */
+    size_t j = mid + 1; /* Index for right subarray */
+    size_t k = left;    /* Index for merged array */
+
     printf("Merging...\n");
     printf("[left]: ");
-    for (size_t l = left; l < mid; l++)
+    for (size_t l = left; l <= mid; l++)
     {
         printf("%d", array[l]);
-        if (l < mid - 1)
+        if (l < mid)
+            printf(", ");
+    }
+    printf("\n[right]: ");
+    for (size_t l = mid + 1; l <= right; l++)
+    {
+        printf("%d", array[l]);
+        if (l < right)
             printf(", ");
     }
     printf("\n");
 
-    printf("[right]: ");
-    for (size_t l = mid; l < right; l++)
-    {
-        printf("%d", array[l]);
-        if (l < right - 1)
-            printf(", ");
-    }
-    printf("\n");
-    
-    while (i < mid && j < right)
+    /* Merge the two subarrays */
+    while (i <= mid && j <= right)
     {
         if (array[i] <= array[j])
             temp[k++] = array[i++];
         else
             temp[k++] = array[j++];
     }
-    
-    while (i < mid)
+
+    /* Copy remaining elements from left subarray */
+    while (i <= mid)
         temp[k++] = array[i++];
-    
-    while (j < right)
+
+    /* Copy remaining elements from right subarray */
+    while (j <= right)
         temp[k++] = array[j++];
-    
-    for (i = left; i < right; i++)
+
+    /* Copy merged elements back to original array */
+    for (i = left; i <= right; i++)
         array[i] = temp[i];
-    
+
     printf("[Done]: ");
-    for (i = left; i < right; i++)
+    for (i = left; i <= right; i++)
     {
         printf("%d", array[i]);
-        if (i < right - 1)
+        if (i < right)
             printf(", ");
     }
     printf("\n");
 }
 
 /**
- * merge_sort_recursive - Recursive implementation of the merge sort algorithm
+ * merge_sort_recursive - Recursively divides and sorts array using merge sort
+ * @array: Array to be sorted
+ * @temp: Temporary array for merging
+ * @left: Start index of the array
+ * @right: End index of the array
  */
 void merge_sort_recursive(int *array, int *temp, size_t left, size_t right)
 {
-    if (right - left > 1)
+    if (left < right)
     {
-        size_t mid = left + (right - left) / 2 + (right - left) % 2;
+        /* Calculate middle point, ensuring left side is <= right side */
+        size_t mid = left + (right - left) / 2;
+
+        /* Sort left subarray */
         merge_sort_recursive(array, temp, left, mid);
-        merge_sort_recursive(array, temp, mid, right);
+        
+        /* Sort right subarray */
+        merge_sort_recursive(array, temp, mid + 1, right);
+
+        /* Merge the sorted subarrays */
         merge(array, temp, left, mid, right);
     }
 }
 
 /**
- * merge_sort - Sorts an array of integers in ascending order using Merge sort
+ * merge_sort - Sorts an array of integers in ascending order using Merge Sort
+ * @array: Array to be sorted
+ * @size: Size of the array
  */
 void merge_sort(int *array, size_t size)
 {
+    int *temp;
+
     if (!array || size < 2)
         return;
-    
-    int *temp = malloc(sizeof(int) * size);
+
+    /* Allocate memory for temporary array once */
+    temp = malloc(sizeof(int) * size);
     if (!temp)
         return;
-    
-    merge_sort_recursive(array, temp, 0, size);
-    
+
+    /* Call recursive merge sort function */
+    merge_sort_recursive(array, temp, 0, size - 1);
+
+    /* Free allocated memory */
     free(temp);
 }
